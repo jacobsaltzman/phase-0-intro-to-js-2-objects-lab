83d5346df1cb4f7dc65e5e4136283a16aac1186b
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-03 18:59:13 -0700

    commit tree ca876d528946f59f0eb7fbfdce45f5ba3c919af8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Steve",
+    streetAddress: 90210
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, name, streetAddress){
+        obj[name] = streetAddress;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name;
+    return employee;
+}
